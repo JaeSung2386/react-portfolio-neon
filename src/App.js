24032d99d91685af14ad7ab40e41b610f372518b
강재성 <wotjd2386@gmail.com>
2024-03-11 23:34:11 +0900

    Chore : react-chrono 설치'

--- src/App.js
+++ src/App.js
@@ -88,35 +88,6 @@ function App() {
 
       {/* Footer Start */}
       <Footer />
-      {/* <footer>
-        <div className="footer">
-          <div className="copyright">
-            <h3>Developed by Kang Jae Seong</h3>
-          </div>
-          <div className="copyright">
-            <h3>Copyright © 2024</h3>
-          </div>
-          <div className="footer-social-icons">
-            <ul className="social-icons">
-              <li>
-                <a href="mailto:wotjd2386@gmail.com">
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/JaeSung2386" target="_blank">
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-              </li>
-              <li>
-                <a href="https://webveloper.dev/" target="_blank">
-                  <FontAwesomeIcon icon={faWordpressSimple} />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </footer> */}
       {/* Footer End */}
     </div>
   );
