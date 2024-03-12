412206d7228fd86fe2a270e39cab43f97fe8a9ce
강재성 <kangjaeseong@A1-2.local>
2024-03-12 20:56:50 +0900

    Feat : Styles 컴포넌트 생성성

---
+++ src/components/Skils/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// 목록
+// 1. 실무 사용 및 경험
+// 2. 개인 스터디 목적
+
+// 차트
+// 1. 실무 역량
+// 2. 문법 이해
+// 3. 간략 설명
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
