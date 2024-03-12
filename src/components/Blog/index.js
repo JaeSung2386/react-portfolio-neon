16dbafe84921c108e53299ce1d9acfbefd7de96c
강재성 <kangjaeseong@A1-2.local>
2024-03-12 20:27:02 +0900

    Style : 블로그 컴포넌트 텍스트 작업중...

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {
+  faLink
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 import '../../assets/styles/blog.css';
 import NaverBlogLogo from '../../assets/imgs/naver_blog_logo.png';
@@ -52,10 +57,13 @@ const index = () => {
       {/* <div 
         style={{padding: "10px 0px 10px 0px"}}
       /> */}
-
+{/* https://www.npmjs.com/package/react-tooltip */}
       <div className="box">
-        <div style={{display: "flex",
-            padding: "10px"}}>
+        <div style={{
+            display: "flex",
+            padding: "20px",
+            gap: "20px"
+            }}>
           <div>
             <img
               src={TistoryLogo}
@@ -63,13 +71,18 @@ const index = () => {
               // className="img-profile"
             />
           </div>
-          <div>
-            <p>asdfsadf</p>
-            <p>asdfsadf</p>
-            <p>asdfsadf</p>
-            <p>asdfsadf</p>
-            <p>asdfsadf</p>
-            <p>asdfsadf</p>
+          <div style={{color: "#FFF"}}>
+            <h1>
+              Tistory
+            </h1>
+            <ul style={{
+              listStylePosition: "inside"
+            }}>
+              <li>운영 기간 : 2020-05-03 ~ 2023-09-22</li>
+              <li>누적 방문수 : 300만 이상</li>
+              <li>포스팅 수 : 787개</li>
+              <li>취업 후 이때까지 배운 지식들을 기록하기 위해 시작한 블로그입니다.</li>
+            </ul>
           </div>
         </div>
       </div>
